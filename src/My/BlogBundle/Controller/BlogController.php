1c7b107c0a29d64d742663577a86011bfd9670ed
46goma <red4193@gmail.com>
2013-05-21 02:47:04 +0900

    記事を編集できるようにした
    編集ボタンとかも足した〜

--- src/My/BlogBundle/Controller/BlogController.php
+++ src/My/BlogBundle/Controller/BlogController.php
@@ -83,4 +83,47 @@ class BlogController extends Controller
             'form' => $form->createView(),
         );
     }
+    /**
+     * editAction 
+     * 
+     * @param Request $request 
+     * @param int $id
+     * @access public
+     * @return void
+     * @Route("/edit/{id}", name="blog_edit")
+     */
+    public function editAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository('MyBlogBundle:Post')->find($id);
+        if (!$post) {
+            throw $this->createNotFoundException('The post does not exist');
+        }
+
+        // form build
+        $form = $this->createFormBuilder($post)
+            ->add('title')
+            ->add('body')
+            ->getForm();
+
+        if ('POST' === $request->getMethod()) {
+            $form->bind($request);
+            // validation
+            if ($form->isValid()) {
+                // エンティティを永続化
+                $post = $form->getData();
+                $post->setCreatedAt(new \DateTime());
+                $post->setUpdatedAt(new \DateTime());
+                $em = $this->getDoctrine()->getManager();
+                $em->flush();
+                
+                return $this->redirect($this->generateUrl('blog_index'));
+            }
+        }
+
+        return array(
+            'post' => $post,
+            'form' => $form->createView(),
+        );
+    }
 }
